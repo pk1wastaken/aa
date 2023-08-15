b95ad53c9b0b04a30b9dd99504f78cd7dd05f1a7
pk1wastaken <117990545+pk1wastaken@users.noreply.github.com>
2023-08-16 02:54:44 +0530

    Update bell.java

--- bell.java
+++ bell.java
@@ -3,12 +3,14 @@ import java.util.Arrays;
 public class BellmanFord {
 
     public static void main(String[] args) {
+        int INFINITY = Integer.MAX_VALUE;
+
         int[][] graph = {
-            {0, 6, Integer.MAX_VALUE, Integer.MAX_VALUE, 7},
-            {Integer.MAX_VALUE, 0, 5, 4, 8},
-            {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, Integer.MAX_VALUE},
-            {2, Integer.MAX_VALUE, 7, 0, Integer.MAX_VALUE},
-            {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 9, 0}
+            {0, 6, INFINITY, INFINITY, 7},
+            {INFINITY, 0, 5, 4, 8},
+            {INFINITY, INFINITY, 0, INFINITY, INFINITY},
+            {2, INFINITY, 7, 0, INFINITY},
+            {INFINITY, INFINITY, INFINITY, 9, 0}
         };
         
         int startNode = 0;
@@ -17,20 +19,21 @@ public class BellmanFord {
         
         System.out.println("Shortest distances from node " + startNode + ":");
         for (int i = 0; i < distances.length; i++) {
-            System.out.println("Node " + i + ": " + (distances[i] == Integer.MAX_VALUE ? "Infinity" : distances[i]));
+            System.out.println("Node " + i + ": " + (distances[i] == INFINITY ? "Infinity" : distances[i]));
         }
     }
 
     public static int[] bellmanFord(int[][] graph, int start) {
         int n = graph.length;
+        int INFINITY = Integer.MAX_VALUE;
         int[] distances = new int[n];
-        Arrays.fill(distances, Integer.MAX_VALUE);
+        Arrays.fill(distances, INFINITY);
         distances[start] = 0;
 
         for (int i = 0; i < n - 1; i++) {
             for (int u = 0; u < n; u++) {
                 for (int v = 0; v < n; v++) {
-                    if (graph[u][v] != Integer.MAX_VALUE && distances[u] != Integer.MAX_VALUE && distances[u] + graph[u][v] < distances[v]) {
+                    if (graph[u][v] != INFINITY && distances[u] != INFINITY && distances[u] + graph[u][v] < distances[v]) {
                         distances[v] = distances[u] + graph[u][v];
                     }
                 }
