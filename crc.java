2276e0d99ef8070f9ff7cf594ddf948d50198b9b
pk1wastaken <117990545+pk1wastaken@users.noreply.github.com>
2023-08-16 07:39:40 +0530

    Update crc.java

--- crc.java
+++ crc.java
@@ -1,32 +1,106 @@
-import java.util.Scanner;
+import java.io.*;
 
-class CRC {
-    void div(int a[], int k) {
-        int gp[] = {1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1};
-        for (int i = 0; i < k; i++)
-            if (a[i] == gp[0])
-                for (int j = i, c = 0; j < 17 + i; j++)
-                    a[j] ^= gp[c++];
+class Crc {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] data;
+        int[] div;
+        int[] divisor;
+        int[] rem;
+        int[] crc;
+        int data_bits, divisor_bits, tot_length;
+
+        System.out.println("Enter number of data bits: ");
+        data_bits = Integer.parseInt(br.readLine());
+        data = new int[data_bits];
+
+        System.out.println("Enter data bits: ");
+        for (int i = 0; i < data_bits; i++)
+            data[i] = Integer.parseInt(br.readLine());
+
+        System.out.println("Enter number of bits in divisor: ");
+        divisor_bits = Integer.parseInt(br.readLine());
+        divisor = new int[divisor_bits];
+
+        System.out.println("Enter Divisor bits: ");
+        for (int i = 0; i < divisor_bits; i++)
+            divisor[i] = Integer.parseInt(br.readLine());
+
+        tot_length = data_bits + divisor_bits;
+        div = new int[tot_length];
+        rem = new int[tot_length];
+        crc = new int[tot_length];
+
+        /* ------------------ CRC GENERATION----------------------- */
+        for (int i = 0; i < data.length; i++)
+            div[i] = data[i];
+
+        System.out.print("Dividend (after appending 0's) are: ");
+        for (int i = 0; i < div.length; i++)
+            System.out.print(div[i]);
+        
+        System.out.println();
+
+        for (int j = 0; j < div.length; j++) {
+            rem[j] = div[j];
+        }
+
+        rem = divide(div, divisor, rem);
+
+        for (int i = 0; i < div.length; i++) {
+            crc[i] = (div[i] ^ rem[i]);
+        }
+
+        System.out.println();
+        System.out.println("CRC code: ");
+        for (int i = 0; i < crc.length; i++)
+            System.out.print(crc[i]);
+
+        /* ------------------- ERROR DETECTION --------------------- */
+        System.out.println();
+        System.out.println("Enter CRC code of " + tot_length + " bits: ");
+
+        for (int i = 0; i < crc.length; i++)
+            crc[i] = Integer.parseInt(br.readLine());
+
+        for (int j = 0; j < crc.length; j++) {
+            rem[j] = crc[j];
+        }
+
+        rem = divide(crc, divisor, rem);
+
+        boolean isError = false;
+
+        for (int i = 0; i < rem.length; i++) {
+            if (rem[i] != 0) {
+                isError = true;
+                break;
+            }
+        }
+
+        if (isError) {
+            System.out.println("Error");
+        } else {
+            System.out.println("No Error");
+        }
+
+        System.out.println("THANK YOU.... :)");
     }
 
-    public static void main(String args[]) {
-        int a[] = new int[66], b[] = new int[66], len, k, f = 0;
-        PRO7 o = new PRO7();
-        Scanner s = new Scanner(System.in);
-        len = s.nextInt();
-        for (int i = 0; i < len; i++) a[i] = s.nextInt();
-        for (int i = 0; i < 16; i++) a[len++] = 0;
-        k = len - 16;
-        System.arraycopy(a, 0, b, 0, len);
-        o.div(a, k);
-        for (int i = 0; i < len; i++) a[i] ^= b[i];
-        System.out.println("Data: ");
-        for (int i = 0; i < len; i++) System.out.print(a[i] + " ");
-        System.out.println("\nEnter Received Data: ");
-        for (int i = 0; i < len; i++) a[i] = s.nextInt();
-        o.div(a, k);
-        for (int i = 0; i < len; i++) if (a[i] != 0) f = 1;
-        if (f == 1) System.out.println("error");
-        else System.out.println("no error");
+    static int[] divide(int div[], int divisor[], int rem[]) {
+        int cur = 0;
+        
+        while (true) {
+            for (int i = 0; i < divisor.length; i++)
+                rem[cur + i] = (rem[cur + i] ^ divisor[i]);
+
+            while (rem[cur] == 0 && cur != rem.length - 1)
+                cur++;
+
+            if ((rem.length - cur) < divisor.length)
+                break;
+        }
+
+        return rem;
     }
 }
